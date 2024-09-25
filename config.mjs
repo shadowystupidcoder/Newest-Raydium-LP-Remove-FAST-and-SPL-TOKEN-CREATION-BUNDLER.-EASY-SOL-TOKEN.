bb6c5b77b304a7a25831a521b6ce50c643cffc6e
shadowystupidcoder <154037987+shadowystupidcoder@users.noreply.github.com>
2024-09-25 11:02:50 -0700

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-export const privateKey = "  " // Keypair or Private Key
+export const privateKey = "5o26tnWZxJzWwyoDRawHLsFzroNSJFUxyzBcrpSmenQXC1WWnhmEK368SZ65Hyy2Grp..." // OR "[123, 123, 123, ...]"
 export const rpcUrl = "https://mainnet.helius-rpc.com/?api-key=ee399b7e-ff38-44c9-803b-958fa32ef924" // RPC URL
-export const lpMint = " " // LP MINT NOT TOKEN MINT
 export const jitoTip = 0.001 // amount in SOL
+export const lpMintOrTokenMint = "28j3EfKpqRoKF3jr7rHKya6BWi7DDf2URQgQBwyKbTfZ" // LP MINT OR TOKEN MINT (NOT WSOL, THE OTHER TOKEN OF THE PAIR)
